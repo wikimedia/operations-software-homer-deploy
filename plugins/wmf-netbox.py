a03d7cd16c8574754a9d2fa71d2ef58fc76eed4b
Riccardo Coccioli <rcoccioli@wikimedia.org>
2020-04-06 18:16:22 +0200

    Add Homer's plugins
    * Add a plugins directory for Homer's plugins to be injected at deploy
      time.
    * Add a stub of WMF-specific Netbox plugin.
    * Inject the plugins directory as a homer_plugins package into the
      virtualenv at deploy time.
    
    Change-Id: I19395b8bb522782980716e64ddbbbc4bb55d1a56

---
+++ plugins/wmf-netbox.py
@@ -0,0 +1,7 @@
+"""WMF-specific Homer plugin for Netbox data."""
+from homer.netbox import BaseNetboxDeviceData
+
+
+class NetboxDeviceDataPlugin(BaseNetboxDeviceData):
+    """Device-specific Netbox data class."""
+
