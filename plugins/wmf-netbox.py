e86e30afb604dd67b878b8805df548bca2003f65
Arzhel Younsi <ayounsi@wikimedia.org>
2022-05-16 14:03:47 +0200

    wmf-netbox: create _get_junos_interfaces
    Splitting I98e15f744a1e5c67e2ab99baf64585fc31fdcf23
    
    _get_junos_switch/router_interfaces and _get_disabled to be removed once
    all the templates use the new function
    
    This change should be a NOOP as it doesn't change the existing
    
    Change-Id: I3fbdc8bdd7d30cc50a0f1b47ad96fea8a87ea7c2

--- plugins/wmf-netbox.py
+++ plugins/wmf-netbox.py
@@ -393,3 +393,127 @@ class NetboxDeviceDataPlugin(BaseNetboxDeviceData):  # pylint: disable=too-many-
         # Wait to be done iterating over all the device' interfaces
         # in case an exact match is found after the parent
         return mtu
+
+
+    def _get_junos_interfaces(self):
+        """Expose Netbox interfaces in a way that can be efficiently used by a junos template."""
+        jri = {}  # Junos interfaces
+        lags_members = defaultdict(list)  # List all the lags to find mixed ones
+        ignore_interfaces = ['fxp0-re0', 'fxp0-re1']  # Those are managed in `set groups`
+        # Here, unlike for switches, we don't try to group interfaces together but set the proper attributes directly
+        for nb_int in self.fetch_device_interfaces():
+            # Regarless of what kind of interface it is, set attributes in a Juniper-ish tree
+            interface_config = {}
+            # TODO skip the ones we don't want
+            interface_config['enabled'] = nb_int.enabled
+            interface_name = nb_int.name
+            if interface_name in ignore_interfaces:
+                continue
+            # Ignore VC links
+            if interface_name.startswith('vcp'):
+                continue
+
+            interface_config['description'] = self.interface_description(interface_name)
+            interface_config['type'] = nb_int.type.value
+
+            if nb_int.lag:
+                interface_config['lag'] = nb_int.lag.name
+                jri[interface_name] = interface_config
+                # We store the interfaces contributing to a specific LAG, because if they are of different types,
+                # We will later need to apply the "link-speed mixed" config option.
+                lags_members[nb_int.lag.name].append(interface_name)
+                continue
+            # For a LAG, the MTU is set on the ae (virtual) interface not the physical ones (bundle members)
+            interface_config['mtu'] = self.interface_mtu(interface_name)
+
+            if nb_int.mac_address:  # Set the MAC if any in netbox
+                interface_config['mac'] = nb_int.mac_address
+
+            if nb_int.mode:  # If the interface is tagged or access
+                interface_config['mode'] = nb_int.mode.value
+                # We keep the tagged vlan names
+                interface_vlans = set()
+
+                # Interface is set to access but doesn't have any vlan configured: set to default
+                if interface_config['mode'] == 'access' and not nb_int.untagged_vlan:
+                    interface_vlans.add('default')
+
+                # If any tagged interfaces add them to the list
+                if nb_int.mode.value == 'tagged':
+                    for tagged_vlan in nb_int.tagged_vlans:
+                        interface_vlans.add(tagged_vlan.name)
+
+                # If there is an untagged interface, add it to the list
+                # Either it's a trunked interface, in that case it will be with the other vlans
+                # Or it's an access interface and it will be alone
+                if nb_int.untagged_vlan:
+                    interface_vlans.add(nb_int.untagged_vlan.name)
+                    # Junos needs the native vlan ID and not the name
+                    if nb_int.mode.value == 'tagged':
+                        interface_config['native_vlan_id'] = nb_int.untagged_vlan.vid
+
+                interface_config['vlans'] = list(interface_vlans)
+
+
+            # Assign the IPs to the interface if any
+            if nb_int.count_ipaddresses > 0:
+                # assumes there is v4 for everything
+                interface_config['ips'] = {4: {}, 6: {}}
+
+                virt_ips = {}
+                for ip_address in self.fetch_device_ip_addresses():
+                    if ip_address.assigned_object.name != interface_name:
+                        # Only care about the IPs for our interface
+                        continue
+                    if ip_address.role:
+                        # If we're dealing with a virtual IP, keep it on the side to later on make it a child
+                        # of the real interface IP
+                        if ip_address.role.value == 'vrrp':
+                            virt_ips[ip_address.address] = ip_address.custom_fields['group_id']
+                            continue
+                        if ip_address.role.value == 'anycast':
+                            virt_ips[ip_address.address] = None
+                            continue
+                    interface_config['ips'][ip_address.family.value][ip_interface(ip_address.address)] = {}
+
+                # Now assign any VRRP/Anycast IP to the real interface,
+                # for that we need to find IPs belonging in the same subnet
+                for family, int_ips in interface_config['ips'].items():
+                    for int_ip in int_ips.keys():
+                        for virt_ip, vrrp_group in virt_ips.items():
+                            if ip_interface(virt_ip) in int_ip.network:
+                                if vrrp_group or vrrp_group == 0:
+                                    interface_config['ips'][family][int_ip]['vrrp'] = {ip_interface(virt_ip).ip: vrrp_group}
+                                else:
+                                    interface_config['ips'][family][int_ip]['anycast'] = ip_interface(virt_ip).ip
+                                    interface_config['anycast_gw'] = True
+
+            # Now that we have all the interface or sub-interface attribute,
+            # we need to nest the sub interfaces in the interfaces when needed
+            if '.' in interface_name and interface_name.split('.')[0] in jri:
+                # It's a sub-interface and we already have the parent interface, add the sub interface
+                parent, sub = interface_name.split('.', 1)
+                if 'sub' not in jri[parent]:
+                    jri[parent]['sub'] = {}
+                jri[parent]['sub'][sub] = interface_config
+            # If we have the sub interface but not the parent yet
+            elif '.' in interface_name and interface_name.split('.')[0] not in jri:
+                parent, sub = interface_name.split('.', 1)
+                # Create the parent
+                jri[parent] = {'sub': {sub: interface_config}}
+            # If we have the parent interface but we found a sub interface earlier
+            elif '.' not in interface_name and interface_name in jri:
+                # Merge the new stuff with the existing one
+                jri[interface_name].update(interface_config)
+            # Easiest case, we find the parent interface first
+            elif '.' not in interface_name and interface_name not in jri:
+                jri[interface_name] = interface_config
+
+            # TODO Remove some Juniper oddities, eg. sub interfaces for LAG members
+
+        # Check if there is any mixed LAG
+        for lag, members in lags_members.items():
+            if len(set([member.split('-')[0] for member in members])) > 1:
+                jri[lag]['mixed'] = True
+
+        return jri
