6c9ebd191005e420706f3fd26944703d0df51913
Arzhel Younsi <ayounsi@wikimedia.org>
2022-05-16 14:29:00 +0200

    wmf-netbox: prefix disabled interfaces with DISABLED
    Splitting I98e15f744a1e5c67e2ab99baf64585fc31fdcf23
    
    Currently a NOOP, will be used when we stop using interface-ranges
    
    Change-Id: I53f71ea68570abc2db6d719446eb1a94cc256cf2

--- plugins/wmf-netbox.py
+++ plugins/wmf-netbox.py
@@ -275,11 +275,14 @@ class NetboxDeviceDataPlugin(BaseNetboxDeviceData):  # pylint: disable=too-many-
     def interface_description(self, interface_name: str):
         """Generate an interface description based on multiple factors (remote endpoint, Netbox description, etc)."""
         description = None
+        prefix = ''
         a_int = None  # A (local) side interface of a cable
         for nb_int in self.fetch_device_interfaces():
             if nb_int.name == interface_name:  # If we have an exact interface match
+                if not nb_int.enabled:
+                    prefix = 'DISABLED '
                 if nb_int.description:  # Return description if any
-                    return nb_int.description
+                    return prefix + nb_int.description
                 elif nb_int.cable:  # Or if it's connected, save the interface for later
                     a_int = nb_int
                     break
@@ -290,11 +293,11 @@ class NetboxDeviceDataPlugin(BaseNetboxDeviceData):  # pylint: disable=too-many-
                     a_int = nb_int
         # Only return the parent description once we're sure there is no exact match with a description
         if description:
-            return description
+            return prefix + description
 
         # If really found nothing, return it
         if not a_int:
-            return ''
+            return prefix.strip()
 
         # Now we can focus on finding the Z side, and there are different scenarios
         # An interface can be connected (using a cable) to:
@@ -325,7 +328,7 @@ class NetboxDeviceDataPlugin(BaseNetboxDeviceData):  # pylint: disable=too-many-
             if z_int:
                 z_int = f":{z_int}"
             description = f"{link_type}{z_dev}{z_int}{cable_label}"
-            return description
+            return prefix + description
 
         elif a_int.cable_peer_type == 'circuits.circuittermination':
             # Constant variables regadless of the # of terminations
@@ -375,7 +378,7 @@ class NetboxDeviceDataPlugin(BaseNetboxDeviceData):  # pylint: disable=too-many-
                 z_int = connected_endpoint.name
 
                 description = f"{link_type}: {z_dev}:{z_int} ({provider}, {', '.join(details)}) {{#{cable_label}}}"
-            return description
+            return prefix + description
 
     # If the specific (sub)interface has a non default MTU: return that
     # Else try to find the parent interface MTU
